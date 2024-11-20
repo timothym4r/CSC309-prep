baed0c8824def65e33f296bc543175ef2923cf27
Timothy <timothymarcellopasaribu@gmail.com>
2024-11-20 16:43:05 -0500

    adding csc309-e11.js

--- pages/csc309-e11.js
+++ pages/csc309-e11.js
@@ -1,4 +1,7 @@
-<div id="auto-tester">
-  Hi. I am pasarib1, and I have learned how to develop a full-stack web app
-  and deploy it!
-</div>
+
+export default function func(){
+    return (<div id="auto-tester">
+        Hi. I am pasarib1, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>)
+}
